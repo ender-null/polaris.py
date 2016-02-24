04cb94d98c8c677bfd76cbb448146db9bba270c2
Ender <luksi.reiku@gmail.com>
2016-02-24 07:53:07 +0100

    Fixed a little bug of the tg bindings.

--- core/bindings/tg.py
+++ core/bindings/tg.py
@@ -47,13 +47,13 @@ def inbox_listen():
                     if hasattr(msg['receiver'], 'last_name'):
                         receiver.last_name = msg['receiver']['last_name']
                     receiver.username = msg['receiver']['username']
-                    receiver.id = msg['receiver']['id']
+                    receiver.id = int(msg['receiver']['id'])
                 else:
                     receiver = Group
                     receiver.title = msg['receiver']['title']
-                    receiver.id = - msg['receiver']['id']
+                    receiver.id = - int(msg['receiver']['id'])
                 sender = User
-                sender.id = msg['sender']['id']
+                sender.id = int(msg['sender']['id'])
                 sender.first_name = msg['sender']['first_name']
                 if hasattr(msg['sender'], 'last_name'):
                     sender.last_name = msg['sender']['last_name']
